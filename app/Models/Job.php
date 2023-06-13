d22e135e09f486798492b285cc14e7807fadb574
TacitReturn <glennrudge@outlook.com>
2023-06-12 23:49:55 -0400

    Initial commit

---
+++ app/Models/Job.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
+{
+    use HasFactory;
+
+    public static array $experience = ["Beginner", "Intermediate", "Senior"];
+    public static array $category = [
+        "IT",
+        "Finance",
+        "Sales",
+        "Marketing",
+    ];
+}
