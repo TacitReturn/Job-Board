8a77694b5ff315d5e80f62e59b8a9b01c5980586
TacitReturn <glennrudge@outlook.com>
2023-06-17 22:57:24 -0400

    Create button.blade.php and update index.blade.php

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,14 +1,14 @@
 <x-layout>
     <x-breadcrumbs :$jobs class="mb-4" :links="['Jobs' => route('jobs.index')]"/>
-    <x-card class="mb-4 text-sm">
-        <form id="filtering-form" action="{{ route('jobs.index') }}" method="GET">
+    <x-card class="mb-4 text-sm" x-data="">
+        <form x-ref="filters" id="filtering-form" action="{{ route('jobs.index') }}" method="GET">
             <div class="mb-4 grid grid-cols-2 gap-4">
                 <div>
                     <div class="mb-1 font-semibold">
                         Search
                     </div>
                     <x-text-input
-                        form-id="filtering-form"
+                        form-ref="filters"
                         name="search"
                         value="{{ request('search') }}"
                         placeholder="Search for any job..." />
@@ -19,15 +19,16 @@
                     </div>
                     <div class="flex space-x-2">
                         <x-text-input
-                            form-id="filtering-form"
+                            form-ref="filters"
                             name="min_salary"
                             value="{{ request('min_salary') }}"
                             placeholder="From" />
                         <x-text-input
-                            form-id="filtering-form"
+                            form-ref="filters"
                             name="max_salary"
                             value="{{ request('max_salary') }}"
-                            placeholder="To" />
+                            placeholder="To"
+                            form-ref="filters"/>
                     </div>
                 </div>
                 <div>
